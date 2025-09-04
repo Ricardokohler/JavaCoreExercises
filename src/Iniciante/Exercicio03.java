60d7c5aebafc6d8289be8b0825f1c73ded07423e
Ricardo Kohler <ricardokohler@github.com>
2025-09-05 03:06:00 +0800

    git pull test

--- src/Iniciante/Exercicio03.java
+++ src/Iniciante/Exercicio03.java
@@ -6,11 +6,16 @@ public class Exercicio03 {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("-------\nPrograma calculo subtracao de produto:/nFormula ---> (A*B)-(C*D)");
-        double a = sc.nextInt();
-        double b = sc.nextInt();
-        double sub = a - b;
-        System.out.println("subtracao = " + sub);
+        System.out.print("-------\nPrograma calculo subtracao de produto.\n\nDigite o valor de A: ");
+        double a = sc.nextDouble();
+        System.out.print("\nDigite o valor de B: ");
+        double b = sc.nextDouble();
+        System.out.print("\nDigite o valor de C: ");
+        double c = sc.nextDouble();
+        System.out.print("\nDigite ovalor de D: ");
+        double d = sc.nextDouble();
+        double res = (a * b) - (c * d);
+        System.out.println("\nResultado = " + res);
     }
     
 }               
