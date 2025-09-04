ea5ddb9bc4420217a745772cab6bc707222ef504
Ricardo Kohler <ricardokohler@github.com>
2025-09-05 03:54:45 +0800

    Exercise03 - Solution

--- src/Iniciante/Exercicio03.java
+++ src/Iniciante/Exercicio03.java
@@ -1,4 +1,4 @@
-package JavaCoreExercises.src.Iniciante;
+package Iniciante;
 
 import java.util.Scanner;
 
@@ -6,7 +6,7 @@ public class Exercicio03 {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("-------\nPrograma calculo subtracao de produto.\n\nDigite o valor de A: ");
+        System.out.print("-----------------\n\nPrograma calculo subtracao de produto.\n\nDigite o valor de A: ");
         double a = sc.nextDouble();
         System.out.print("\nDigite o valor de B: ");
         double b = sc.nextDouble();
