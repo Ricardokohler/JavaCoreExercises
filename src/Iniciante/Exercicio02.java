823ada42e571ceb7ff9132e9c5f4a2d3358f02f8
Ricardo Kohler <ricardokohler@github.com>
2025-09-05 03:53:44 +0800

    Exercise 2 - Solution

--- src/Iniciante/Exercicio02.java
+++ src/Iniciante/Exercicio02.java
@@ -1,4 +1,4 @@
-package JavaCoreExercises.src.Iniciante;
+package Iniciante;
 
 import java.util.Scanner;
 
@@ -7,7 +7,7 @@ public class Exercicio02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("\n\n----------------");
+        System.out.println("----------------\n\n");
         System.out.print("Programa calculo de area de CIRCULO\nDigite o raio: ");
         Double raio = sc.nextDouble();
         Double area = 3.14159*(Math.pow(raio, 2)); 
