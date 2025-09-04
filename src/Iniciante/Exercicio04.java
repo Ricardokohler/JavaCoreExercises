d1e044aef9f8095556e91dabf3d2550d07dabed3
Ricardo Kohler <ricardokohler@github.com>
2025-09-05 03:58:12 +0800

    Exercise 05 - Solution

--- src/Iniciante/Exercicio04.java
+++ src/Iniciante/Exercicio04.java
@@ -1,9 +1,24 @@
-package JavaCoreExercises.src.Iniciante;
+package Iniciante;
+
+import java.util.Scanner;
 
 public class Exercicio04 {
     public static void main(String[] args) {
 
-
+    Scanner sc = new Scanner(System.in);
+        System.out.println("-----------------------------------\n");
+        System.out.print("Programa calculo de pagamento para funcionarios\n\nDigite o id do funcionario: ");
+        int id = sc.nextInt();
+        System.out.print("Valor por hora: ");
+        double value = sc.nextDouble();
+        System.out.print("Horas trabalhadas: ");
+        double hours = sc.nextDouble();
+        
+        double payment = value * hours;
+        System.out.print("Pagamento do funcionario: " + payment);
+        
+        
+        
 
     }
 }
