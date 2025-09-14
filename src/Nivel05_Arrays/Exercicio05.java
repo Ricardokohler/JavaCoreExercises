73f5e6a653a2fae2ac48f963e2cb8bcfe9ba33d8
Ricardo Kohler <ricardokohler@github.com>
2025-09-14 18:49:13 -0300

    Update: Exercises(03-06)

--- src/Nivel05_Arrays/Exercicio05.java
+++ src/Nivel05_Arrays/Exercicio05.java
@@ -1,22 +1,39 @@
+
+
 package Nivel05_Arrays;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicio05 {
 
     public static void main(String[] args) {
-
-
-
-    /*               PROBLEMA "MAIOR POSIÇÃO"
-
-Faça um programa que leia N números reais e armazene-os em um vetor.
-Em seguida, mostrar na tela:
-- O maior número do vetor (supor não haver empates).
-- Mostrar também a posição do maior elemento,
-considerando a primeira posição como 0 (zero).
-
-    */
-
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
         
+        System.out.print("\n\nQuantos numeros serao digitados? ");
+        int n = sc.nextInt();
+        
+        int [] nums = new int[n]; 
+        int count = 0;
+        int posit = 0;
+        
+        System.out.print("\nValores: \n");
+        for(int i=0; i<n; i++){
+            System.out.print("Digite um numero: ");
+            nums[i] = sc.nextInt();
+            
+            if(nums[i] > count){
+                count = nums[i];
+                posit = i;
+            }
+            
+        }
+        System.out.print("\nMaior valor: " + count + "\nPosiçao: " + posit +"\n");
+        
+        System.out.print("\nObrigado!\n")
+        
+        sc.close();
     }
 }
 
--- src/Nivel05_Arrays/Exercicio06.java
+++ src/Nivel05_Arrays/Exercicio06.java
@@ -1,11 +1,3 @@
-package Nivel05_Arrays;
-
-public class Exercicio06 {
-
-    public static void main(String[] args) {
-
-
-
     /*               Problema "soma_vetores2"
 
 Faça um programa para ler dois vetores A e B, contendo N elementos cada. Em seguida, gere um
@@ -13,8 +5,47 @@ terceiro vetor C onde cada elemento de C é a soma dos elementos correspondentes
 o vetor C gerado.
 
     */
+package Nivel05_Arrays;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio06 {
+
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
         
+        System.out.print("\n+------------------------------------+");
+        System.out.print("\n|------Programa soma de vetores------|");
+        System.out.print("\n+------------------------------------+");
+        
+        System.out.print("\n\nQuantos numeros serao digitados? ");
+        int n = sc.nextInt();
+        
+        int [] n1 = new int[n];
+        int [] n2 = new int[n];
+        int [] sum = new int[n];
+        
+        System.out.print("\nVetor #1 ");
+        for(int i=0; i<n; i++){
+            System.out.print("\nDigite um numero: ");
+            n1[i] = sc.nextInt();
+        }
+        
+        System.out.print("\nVetor #2: ");
+        for (int i=0; i<n; i++){
+            System.out.print("\nDigite um numero: ");
+            n2[i] = sc.nextInt();    
+        }
+        
+        for(int i =0; i<n; i++){
+            System.out.print("\nSoma: " + (n1[i] + n2[i]));
+        }
+        
+        System.out.print("\nObrigado! \n");
+        sc.close();
     }
 }
 
