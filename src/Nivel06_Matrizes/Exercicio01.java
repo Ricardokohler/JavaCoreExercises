558ba80928d1e48b830bc7e0c85cca135837bbaf
Ricardo Kohler <ricardokohlermk@gmail.com>
2025-09-15 17:22:43 -0300

    Update: Setup(Arrays-Matrizes)

--- src/Nivel06_Matrizes/Exercicio01.java
+++ src/Nivel06_Matrizes/Exercicio01.java
@@ -1,5 +1,8 @@
 package Nivel06_Matrizes;
 
 public class Exercicio01 {
+    public static void main(String[] args) {
 
+
+    }
 }
--- src/Nivel06_Matrizes/Exercicio04.java
+++ src/Nivel06_Matrizes/Exercicio04.java
@@ -1,5 +1,8 @@
 package Nivel06_Matrizes;
 
 public class Exercicio04 {
+    public static void main(String[] args) {
 
+
+    }
 }
