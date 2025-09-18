dc951934ef00d7ae7f34b05f92022b44106b612c
Ricardo Kohler <ricardokohler@github.com>
2025-09-18 20:53:52 -0300

    Update (Solved Exercises)

--- src/Nivel02_Intermediario/Arrays/Exercicio08.java
+++ src/Nivel02_Intermediario/Arrays/Exercicio08.java
@@ -1,17 +1,39 @@
 package Nivel02_Intermediario.Arrays;
-
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio08 {
     public static void main(String[] args) {
+        
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         System.out.print("\n+---------------------------------------+");
         System.out.print("\n|------- Programa média de pares -------|");
-        System.out.print("\n+---------------------------------------+");
-        //caso nao haja pares, printar (NAO HA PARES)
+        System.out.print("\n+---------------------------------------+\n\n");
+        
+        System.out.print("Quantos numeros serao digitados? ");
+        int n = sc.nextInt();
+        
+        Double [] nums = new Double[n];
+        Double avg = 0.0;
+        
+        for(int i=0; i<n; i++){
+            System.out.print("\nDigite um numero: ");
+            nums[i] = sc.nextDouble();
+            if(nums[i]%2==0){
+                avg += nums[i];
+            }
+        }
+        
+        if(avg == 0.0){
+            System.out.print("\n\n>Nenhum numero par\n");
+            sc.close();    
+        } else{
+        
+           avg = avg / n;
+           System.out.printf("\n>Media dos pares: %.1f\n", avg);
+        }
         sc.close();
     }
 }
--- src/Nivel02_Intermediario/Arrays/Exercicio09.java
+++ src/Nivel02_Intermediario/Arrays/Exercicio09.java
@@ -11,9 +11,31 @@ public class Exercicio09 {
         System.out.print("\n+---------------------------------------+");
         System.out.print("\n|-------- Programa mais velho ----------|");
         System.out.print("\n+---------------------------------------+");
-        //
-
-
+        
+        System.out.print("\n\nQuantas pessoas voce vai digitar? ");
+        int n = sc.nextInt();
+        int older = 0;//contador a partir do indice
+        
+        
+        int [] ages = new int[n];
+        String [] names = new String [n];
+        
+        for(int i=0; i<n; i++){
+            sc.nextLine();
+            System.out.print("\nDados da "+(i+1)+"a pessoa:\nNome:");
+            
+            names[i] = sc.nextLine();
+                
+            System.out.print("Idade: ");
+            ages[i] = sc.nextInt();
+            
+            if(ages[i] > ages[older]){
+                older = i;
+            }
+        }    
+        
+        System.out.print("\n\nPessoa mais velha: " + names[older]+ "\n");
+        
         sc.close();
     }
 }
